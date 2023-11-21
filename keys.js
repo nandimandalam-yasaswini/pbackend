8508bf2b38b0593eba493ca81e3722a4e7dd9e22
nandimandalam yasaswini <nyasaswini2004@gmail.com>
2023-11-21 21:10:11 +0530

    first commit

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_URI: "mongodb+srv://erthinenibhavyasree:21Bds0116@library.w5d6gue.mongodb.net/library?retryWrites=true&w=majority",
+    JWT_SECRET: "HGJBNHGBJNHVGBJNHVGBNHGVBNGVHBJNGHVBVGHBFCVGHJGVHGVHGHVGVHN"
+};
